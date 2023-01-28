aa6ea7a68df9d6297894a495298425f65b3cad39
niksorokin07 <117514209+niksorokin07@users.noreply.github.com>
2023-01-28 23:31:00 +0300

    Add files via upload
    1, 2, 4, 5 исправленные Мёрджи

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ map_file2 = "map2.png"
 
 def get_map(toponym_coordinates, zoom, map_file, *pt):
     if pt:
-        pt = ",".join(coords_of_city.split())
+        pt = ",".join(pt[0].split())
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(toponym_coordinates.split())}&spn={','.join(zoom.split())}&l=map&pt={pt},pm2gnm1"
     else:
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(toponym_coordinates.split())}&spn={','.join(zoom.split())}&l=map"
@@ -37,7 +37,7 @@ def get_map(toponym_coordinates, zoom, map_file, *pt):
 
 def get_sat(toponym_coordinates, zoom, map_file, *pt):
     if pt:
-        pt = ",".join(coords_of_city.split())
+        pt = ",".join(pt[0].split())
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(toponym_coordinates.split())}&spn={','.join(zoom.split())}&l=sat&pt={pt},pm2gnm1"
     else:
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(toponym_coordinates.split())}&spn={','.join(zoom.split())}&l=sat"
@@ -53,7 +53,7 @@ def get_sat(toponym_coordinates, zoom, map_file, *pt):
 
 def get_gb(toponym_coordinates, zoom, map_file, *pt):
     if pt:
-        pt = ",".join(coords_of_city.split())
+        pt = ",".join(pt[0].split())
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(toponym_coordinates.split())}&spn={','.join(zoom.split())}&l=sat,skl&pt={pt},pm2gnm1"
     else:
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(toponym_coordinates.split())}&spn={','.join(zoom.split())}&l=sat,skl"
@@ -112,14 +112,12 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             i += 1
             i = (i + 1) % 3 - 1
-
         if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
             zoom = zoom.split()
-            if float(zoom[0]) >= 1 and float(zoom[0]) <= 70 and float(zoom[1]) >= 1 and float(zoom[1]) <= 70:
+            if float(zoom[0]) >= 0 and float(zoom[0]) <= 70 and float(zoom[1]) >= 0 and float(zoom[1]) <= 70:
                 zoom = f'{float(zoom[0]) + 1} {float(zoom[1]) + 1}'
             else:
                 zoom = f'{float(zoom[0])} {float(zoom[1])}'
-
             if i == 0:
                 os.remove(map_file)
                 get_map(coords, zoom, map_file)
@@ -129,14 +127,13 @@ while running:
             elif i == -1:
                 os.remove(map_file2)
                 get_gb(coords, zoom, map_file2)
-
+            slides = [pygame.image.load(map_file), pygame.image.load(map_file1), pygame.image.load(map_file2)]
         if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
             zoom = zoom.split()
             if float(zoom[0]) > 0 and float(zoom[1]) > 0:
                 zoom = f'{float(zoom[0]) - 1} {float(zoom[1]) - 1}'
             else:
                 zoom = f'{float(zoom[0])} {float(zoom[1])}'
-
             if i == 0:
                 os.remove(map_file)
                 get_map(coords, zoom, map_file)
@@ -146,16 +143,15 @@ while running:
             elif i == -1:
                 os.remove(map_file2)
                 get_gb(coords, zoom, map_file2)
-
-        slides = [pygame.image.load(map_file), pygame.image.load(map_file1), pygame.image.load(map_file2)]
+            slides = [pygame.image.load(map_file), pygame.image.load(map_file1), pygame.image.load(map_file2)]
         if event.type == pygame.KEYDOWN:
             if active:
                 if event.key == pygame.K_RETURN:
-                    coords_of_city = get_coords_for_city(str(text))
+                    coords = get_coords_for_city(str(text))
                     text = ''
-                    get_map(coords_of_city, "0.5 0.5", map_file, coords_of_city)
-                    get_sat(coords_of_city, "0.5 0.5", map_file1, coords_of_city)
-                    get_gb(coords_of_city, "0.5 0.5", map_file2, coords_of_city)
+                    get_map(coords, "0.5 0.5", map_file, coords)
+                    get_sat(coords, "0.5 0.5", map_file1, coords)
+                    get_gb(coords, "0.5 0.5", map_file2, coords)
                     slides = [pygame.image.load(map_file), pygame.image.load(map_file1), pygame.image.load(map_file2)]
                 elif event.key == pygame.K_BACKSPACE:
                     text = text[:-1]
